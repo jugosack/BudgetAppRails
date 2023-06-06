0a3aab517dfa014f76a32653b32925b8f18268f1
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-06 15:16:41 +0200

    Add codebase for Entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,15 @@
 class Entity < ApplicationRecord
-end
+    belongs_to :author, class_name: 'User'
+    belongs_to :group
+  
+    validates :name, presence: true
+    validates :amount, presence: true
+  
+    def total_entities(id)
+      total = 0
+      Entity.where(group_id: id).each do |entity|
+        total += entity.amount
+      end
+      total
+    end
+  end

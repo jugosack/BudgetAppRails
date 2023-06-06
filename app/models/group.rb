592d777788bd084475a3b14952232d51fd4db9cc
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-06 15:14:34 +0200

    Add codebase for Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,20 @@
 class Group < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+  has_many :entities
+
+  validates :name, presence: true
+
+  def user_name(author_id)
+    User.find(author_id).name
+  end
+
+  def total_spent(id)
+   entities = Entity.where(group_id: id)
+    total = 0
+    entities.each do |entity|
+      total += entity.amount
+    end
+    total
+  end
+end
 end

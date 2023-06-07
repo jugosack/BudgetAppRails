e4fc019907dada916e2eb8e9bedcf804ea275d78
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-07 14:44:50 +0200

    Add cobase for views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_scope  :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
-
+  resources :users do 
+    resources :groups do
+      resources :entities
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
